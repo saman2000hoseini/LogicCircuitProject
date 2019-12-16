51e36ffdd738bf37d14f5ef1ac99eae917b5b75d
saman2000hoseini <saman2000hoseini@gmail.com>
2019-12-17 02:33:56 +0330

    organisation

---
+++ prj/isim/bloodAbnormalityTest_isim_beh.exe.sim/work/m_00000000002118056088_2869430869.c
@@ -0,0 +1,118 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Saman/LogicCircuitDesignProjectFirstPhase/src/BloodTypeClassification.v";
+static unsigned int ng1[] = {1U, 0U};
+static unsigned int ng2[] = {0U, 0U};
+
+
+
+static void Cont_26_0(char *t0)
+{
+    char t3[8];
+    char t7[8];
+    char *t1;
+    char *t2;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t8;
+    unsigned int t9;
+    unsigned int t10;
+    unsigned int t11;
+    unsigned int t12;
+    unsigned int t13;
+    unsigned int t14;
+    char *t15;
+    char *t16;
+    char *t17;
+    char *t18;
+    char *t19;
+    char *t20;
+    unsigned int t21;
+    unsigned int t22;
+    char *t23;
+    unsigned int t24;
+    unsigned int t25;
+    char *t26;
+    unsigned int t27;
+    unsigned int t28;
+    char *t29;
+
+LAB0:    t1 = (t0 + 2528U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(26, ng0);
+    t2 = ((char*)((ng1)));
+    t4 = ((char*)((ng2)));
+    t5 = (t0 + 1048U);
+    t6 = *((char **)t5);
+    memset(t7, 0, 8);
+    t5 = (t7 + 4);
+    t8 = (t6 + 4);
+    t9 = *((unsigned int *)t6);
+    t10 = (t9 >> 0);
+    t11 = (t10 & 1);
+    *((unsigned int *)t7) = t11;
+    t12 = *((unsigned int *)t8);
+    t13 = (t12 >> 0);
+    t14 = (t13 & 1);
+    *((unsigned int *)t5) = t14;
+    t15 = ((char*)((ng2)));
+    xsi_vlogtype_concat(t3, 4, 4, 4U, t15, 1, t7, 1, t4, 1, t2, 1);
+    t16 = (t0 + 2928);
+    t17 = (t16 + 56U);
+    t18 = *((char **)t17);
+    t19 = (t18 + 56U);
+    t20 = *((char **)t19);
+    memset(t20, 0, 8);
+    t21 = 15U;
+    t22 = t21;
+    t23 = (t3 + 4);
+    t24 = *((unsigned int *)t3);
+    t21 = (t21 & t24);
+    t25 = *((unsigned int *)t23);
+    t22 = (t22 & t25);
+    t26 = (t20 + 4);
+    t27 = *((unsigned int *)t20);
+    *((unsigned int *)t20) = (t27 | t21);
+    t28 = *((unsigned int *)t26);
+    *((unsigned int *)t26) = (t28 | t22);
+    xsi_driver_vfirst_trans(t16, 0, 3);
+    t29 = (t0 + 2848);
+    *((int *)t29) = 1;
+
+LAB1:    return;
+}
+
+
+extern void work_m_00000000002118056088_2869430869_init()
+{
+	static char *pe[] = {(void *)Cont_26_0};
+	xsi_register_didat("work_m_00000000002118056088_2869430869", "isim/bloodAbnormalityTest_isim_beh.exe.sim/work/m_00000000002118056088_2869430869.didat");
+	xsi_register_executes(pe);
+}
---
+++ prj/isim/bloodAbnormalityTest_isim_beh.exe.sim/work/m_00000000003926227663_1413137731.c
@@ -0,0 +1,201 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/Saman/LogicCircuitDesignProjectFirstPhase/src/bloodAbnormalityTest.v";
+static int ng1[] = {0, 0};
+static unsigned int ng2[] = {2U, 0U};
+static unsigned int ng3[] = {0U, 0U};
+static unsigned int ng4[] = {3U, 0U};
+static unsigned int ng5[] = {4U, 0U};
+static unsigned int ng6[] = {5U, 0U};
+static unsigned int ng7[] = {6U, 0U};
+static unsigned int ng8[] = {7U, 0U};
+
+
+
+static void Initial_41_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+
+LAB0:    t1 = (t0 + 2528U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(41, ng0);
+
+LAB4:    xsi_set_current_line(43, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(44, ng0);
+    t2 = ((char*)((ng1)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(47, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB5;
+
+LAB1:    return;
+LAB5:    xsi_set_current_line(51, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(52, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(55, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB6;
+    goto LAB1;
+
+LAB6:    xsi_set_current_line(57, ng0);
+    t2 = ((char*)((ng4)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(58, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(61, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB7;
+    goto LAB1;
+
+LAB7:    xsi_set_current_line(63, ng0);
+    t2 = ((char*)((ng5)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(64, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(67, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB8;
+    goto LAB1;
+
+LAB8:    xsi_set_current_line(69, ng0);
+    t2 = ((char*)((ng6)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(70, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(73, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB9;
+    goto LAB1;
+
+LAB9:    xsi_set_current_line(75, ng0);
+    t2 = ((char*)((ng7)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(76, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(79, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB10;
+    goto LAB1;
+
+LAB10:    xsi_set_current_line(81, ng0);
+    t2 = ((char*)((ng8)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(82, ng0);
+    t2 = ((char*)((ng3)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(85, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB11;
+    goto LAB1;
+
+LAB11:    xsi_set_current_line(87, ng0);
+    t2 = ((char*)((ng6)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(88, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(91, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB12;
+    goto LAB1;
+
+LAB12:    xsi_set_current_line(93, ng0);
+    t2 = ((char*)((ng7)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(94, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(97, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB13;
+    goto LAB1;
+
+LAB13:    xsi_set_current_line(99, ng0);
+    t2 = ((char*)((ng8)));
+    t3 = (t0 + 1448);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 4);
+    xsi_set_current_line(100, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1608);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 3);
+    xsi_set_current_line(103, ng0);
+    t2 = (t0 + 2336);
+    xsi_process_wait(t2, 100000LL);
+    *((char **)t1) = &&LAB14;
+    goto LAB1;
+
+LAB14:    goto LAB1;
+
+}
+
+
+extern void work_m_00000000003926227663_1413137731_init()
+{
+	static char *pe[] = {(void *)Initial_41_0};
+	xsi_register_didat("work_m_00000000003926227663_1413137731", "isim/bloodAbnormalityTest_isim_beh.exe.sim/work/m_00000000003926227663_1413137731.didat");
+	xsi_register_executes(pe);
+}
